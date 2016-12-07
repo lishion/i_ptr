750c3573b1d51df9e939ff10f58a20cec6c57c88
lishion <544670411@qq.com>
2016-12-07 11:14:10 +0800

    empty project with i_ptr class

---
+++ i_ptr.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lishion on 2016/12/7.
+//
+
+#include "i_ptr.h"
---
+++ i_ptr.h
@@ -0,0 +1,14 @@
+//
+// Created by lishion on 2016/12/7.
+//
+
+#ifndef I_PTR_I_PTR_H
+#define I_PTR_I_PTR_H
+
+
+class i_ptr {
+
+};
+
+
+#endif //I_PTR_I_PTR_H
